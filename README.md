0caef86cea0c225a1612f1fafa9ad4543ec8e4a9
toycrane <alwaysfun2183@gmail.com>
2021-03-07 21:15:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# manage-svg-typescript
+ 
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Icon from "./Icon";
+
+function App() {
+	return (
+		<>
+			<Icon size={40} color="#3221BF" icon="camera" />
+			<Icon size={80} color="#C8191F" icon="arrowDown" />
+			<Icon size={160} color="#04BD8D" icon="arrowRight" />
+			<Icon size={320} color="#FDB431" icon="menu" />
+		</>
+	);
+}
+
+export default App;
